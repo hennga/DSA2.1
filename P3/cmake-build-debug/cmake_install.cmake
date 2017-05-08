25beefa7854e2d7deae700c1cd7c368448294afe
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2017-05-08 11:52:52 -0700

    more stuff

--- P3/cmake-build-debug/cmake_install.cmake
+++ P3/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/marcel/CLionProjects/ADS_P3_CLION
+# Install script for directory: /home/marcel/Desktop/DSA2.1/P3
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/marcel/CLionProjects/ADS_P3_CLION/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/marcel/Desktop/DSA2.1/P3/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- P3/install_openmp_gcc.sh
+++ P3/install_openmp_gcc.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 sudo apt-get install gcc-multilib
+sudo apt-get install g++-multilib
