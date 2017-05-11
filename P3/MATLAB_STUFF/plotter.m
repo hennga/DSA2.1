c99a8873bc8dce041ac2916f51c1df53dbcccf7d
RBEGamer <marcel.ochsendorf@gmail.com>
2017-05-12 00:02:14 +0200

    new plotter m

--- P3/MATLAB_STUFF/plotter.m
+++ P3/MATLAB_STUFF/plotter.m
@@ -1,20 +1,30 @@
+base_dir='DATA_SORT/';
+loop_string ='100';
+pron_string = '1000';
+
+heapsort_name= 'heapsort';
+quicksort_name='quicksort';
+mergesort_name='mergesort';
+shellsort_name='shellsort';
+
+
 clear;
 clc; 
 close all;
-fid=fopen('DATA/quicksort.txt');
+fid=fopen([base_dir heapsort_name '/' heapsort_name '_' loop_string '_' pron_string '.txt']);
 data=textscan(fid,'%d %f');
 fclose ( fid );
 x=data{1};
 quicksort_y=data{2};
-fid=fopen('DATA/mergesort.txt');
+fid=fopen([base_dir mergesort_name '/' mergesort_name '_' loop_string '_' pron_string '.txt']);
 data=textscan(fid,'%d %f')
 fclose ( fid );
 mergesort_y=data{2};
-fid=fopen('DATA/heapsort.txt');
+fid=fopen([base_dir heapsort_name '/' heapsort_name '_' loop_string '_' pron_string '.txt']);
 data=textscan(fid,'%d %f');
 fclose ( fid );
 heapsort_y=data{2};
-fid=fopen('DATA/shellsort.txt');
+fid=fopen([base_dir shellsort_name '/' shellsort_name '_' loop_string '_' pron_string '.txt']);
 data=textscan(fid,'%d %f');
 fclose ( fid );
 shellsort_y =data{2};
